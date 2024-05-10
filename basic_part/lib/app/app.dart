187f7aedc134507b72fb081c8835ed9507456430
di <1426567242@qq.com>
2024-05-10 12:53:12 +0800

    自己重写一遍

--- basic_part/lib/app/app.dart
+++ basic_part/lib/app/app.dart
@@ -1,45 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:basic_part/app/components/app_page_header.dart';
-import 'package:basic_part/app/components/app_page_main.dart';
-import 'package:basic_part/app/components/app_page_bottom.dart';
-import 'package:basic_part/app/components/app_floating_action_button.dart';
 
-
-// 常用的组件： StatelessWidget StatefulWidget
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App>{
-  // 显示/隐藏AppBar
-  bool showAppBar = true;
-  int currentAppBottomNavigationBarItem = 0;
-  void onTapAppBottomNavigationBarItem (int index) {
-    currentAppBottomNavigationBarItem = index;
-    setState(() {
-      showAppBar = index == 0;
-    });
-  }
-  
-  Widget build (BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController (
-        length: 2,
-        child: Scaffold(
-          body: AppPageMain(
-            currentIndex: currentAppBottomNavigationBarItem,
-          ),
-          backgroundColor: Colors.black,
-          appBar: showAppBar ? AppPageHeader() : null, // 顶部导航
-          bottomNavigationBar: AppPageBottom(
-            currentIndex: currentAppBottomNavigationBarItem,
-            onTap: onTapAppBottomNavigationBarItem,
-          ),
-          floatingActionButton: AppFloatingActionButton(),
-        )
-      ),
-      debugShowCheckedModeBanner: false,
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('this is test'),
     );
   }
 }
--- basic_part/lib/基本语法/app.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-// 常用的组件： StatelessWidget StatefulWidget
-class App extends StatelessWidget {
-  @override
-  Widget build (BuildContext context) {
-    return Center(child: Text('我是255555app', textDirection: TextDirection.ltr));
-  }
-}
