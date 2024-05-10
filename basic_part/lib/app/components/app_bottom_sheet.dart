ddcbe7479e7e820bcbd986019638ac405b590928
di <1426567242@qq.com>
2024-05-10 10:26:59 +0800

    弹框加警告框

---
+++ basic_part/lib/app/components/app_bottom_sheet.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppBottomSheet extends StatelessWidget {
+  Future<bool?> showAppAplertDialog(context) {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('确认提交'),
+        content: Text('确认提交后不能恢复，确定要提交'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context, false) , 
+            child: Text('取消')
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, true) , 
+            child: Text('确定'),
+          ),
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 350,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.background,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, -20),
+            blurRadius: 30
+          )
+        ]
+      ),
+      child: Center(
+        child: TextButton(
+          child: Text('提交'),
+          onPressed: () async {
+            final result = await showAppAplertDialog(context);
+            print('showAppAplertDialog $result');
+          },
+        ),
+      ),
+    );
+  }
+}
--- basic_part/lib/app/components/app_floating_action_button.dart
+++ basic_part/lib/app/components/app_floating_action_button.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:basic_part/app/components/app_bottom_sheet.dart';
 
-class AppFloatingActionButton extends StatelessWidget {
+class AppFloatingActionButton extends StatefulWidget {
   @override
+  State<AppFloatingActionButton> createState() => _AppFloatingActionButton();
+}
+
+class _AppFloatingActionButton extends State<AppFloatingActionButton> {
+  bool isBottomSheetShown = false;
+  Icon floatingActionButtonIcon () {
+    return isBottomSheetShown ? Icon(Icons.close) : Icon(Icons.support_agent);
+  }
   Widget build (BuildContext context) {
     return FloatingActionButton(
-      child: Icon(Icons.share_outlined),
+      child: floatingActionButtonIcon (),
       onPressed: () {
-        print('floating action button');
+        if (isBottomSheetShown) {
+          return Navigator.pop(context);
+        };
+        final bottomSheetController = showBottomSheet(
+          context: context,
+          builder: (context) => AppBottomSheet()
+        );
+        setState(() {
+          isBottomSheetShown = true;
+        });
+        bottomSheetController.closed.then((value){
+          setState(() {
+            isBottomSheetShown = false;
+          });
+        });
       },
       backgroundColor: Colors.black87,
       foregroundColor: Colors.white70,
+
     );
   }
 }
